0bc084693d4ea2e066151d260c7dd82223068c89
ratHippo <91836375+ratHippo@users.noreply.github.com>
2025-07-25 17:10:39 +0200

    Update Pose.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/Pose.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/Pose.kt
@@ -3,13 +3,32 @@ package org.firstinspires.ftc.teamcode.drivetrain
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D
+import kotlin.math.PI
+import kotlin.math.absoluteValue
 import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.math.sqrt
 
 fun Pose(pose2d: Pose2D) = Pose(pose2d.getX(DistanceUnit.INCH), pose2d.getY(DistanceUnit.INCH), pose2d.getHeading(AngleUnit.RADIANS))
-data class Pose(var x: Double, var y: Double, var heading: Double) {
+
+class Pose(var x: Double, var y: Double, var heading: Double) {
+    fun inSquare(pose: Pose, xTolerance: Double, yTolerance: Double, headingTolerance: Double): Boolean{
+        return (this - pose).inSquare(xTolerance, yTolerance, headingTolerance)
+    }
+    fun inSquare(xTolerance: Double, yTolerance: Double, headingTolerance: Double): Boolean{
+        return  (this.x.absoluteValue < xTolerance) &&
+                (this.y.absoluteValue < yTolerance) &&
+                (this.heading.absoluteValue < headingTolerance)
+    }
+    fun inCircle(pose: Pose, distanceTolerance: Double, headingTolerance: Double): Boolean{
+        return  (this - pose).inCircle(distanceTolerance, headingTolerance)
+    }
+    fun inCircle(distanceTolerance: Double, headingTolerance: Double): Boolean{
+        return  (Vector.fromPose(this).length.absoluteValue < distanceTolerance) &&
+                ((this.heading).absoluteValue < headingTolerance)
+    }
+
     operator fun plus(v: Pose): Pose{
         return Pose(x + v.x, y + v.y, heading+v.heading)
     }
@@ -28,7 +47,20 @@ data class Pose(var x: Double, var y: Double, var heading: Double) {
 };
 
 
-data class Vector private constructor(val angle: Double, val length: Double){
+class Vector {
+    val angle: Double
+    val length: Double
+
+    private constructor(angle: Double, length: Double) {
+        if (length < 0) {
+            this.angle = angle + PI
+            this.length = length.absoluteValue
+        } else {
+            this.angle = angle
+            this.length = length.absoluteValue
+        }
+    }
+
     val x get() = length * sin(angle)
     val y get() = length * cos(angle)
 
@@ -39,6 +71,11 @@ data class Vector private constructor(val angle: Double, val length: Double){
     }
 
     fun norm(): Vector = Vector(angle, 1.0);
+
+    // dot product
+    infix fun dot(v: Vector): Vector {
+        return Vector(v.x * this.x, v.y * this.y)
+    }
 
     operator fun times(x: Number): Vector {
         return Vector(angle, length * x.toDouble());
