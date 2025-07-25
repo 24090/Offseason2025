5dee471324171fe2ebfd437570d3497a01d2d6d7
ratHippo <91836375+ratHippo@users.noreply.github.com>
2025-07-25 17:10:25 +0200

    refactor ControlSystems.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controlsystems/ControlSystems.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controlsystems/ControlSystems.kt
@@ -5,34 +5,19 @@ import kotlin.math.absoluteValue
 import kotlin.math.sign
 import kotlin.math.sqrt
 
-fun PD(error: Double, dError: Double, kP: Double, kD: Double): Double{
-    return error * kP + dError * kD
-}
+fun PD(error: Double, dError: Double, kP: Double, kD: Double) = error * kP + dError * kD
+fun PD(error: Vector, dError: Vector, kP: Double, kD: Double) = error * kP + dError * kD
 
-fun PD(error: Vector, dError: Vector, kP: Double, kD: Double): Vector{
-    return error * kP + dError * kD
-}
+fun PDL(error: Double, dError: Double, kP: Double, kD: Double, kL: Double) = PD(error, dError, kP, kD) + BangBang(error, kL)
+fun PDL(error: Vector, dError: Vector, kP: Double, kD: Double, kL: Double) = PD(error, dError, kP, kD) + BangBang(error, kL)
 
-fun PDL(error: Double, dError: Double, kP: Double, kD: Double, kL: Double): Double{
-    return PD(error, dError, kP, kD) + BangBang(error, kL)
-}
+fun PDLT(error: Vector, dError: Vector, kP: Double, kD: Double, kL: Double, kThresh: Double) =
+    if (error.length.absoluteValue < kThresh)
+        PD(error, dError, kP, kD) 
+    else PDL(error, dError, kP, kD, kL)
 
-fun PDL(error: Vector, dError: Vector, kP: Double, kD: Double, kL: Double): Vector{
-    return PD(error, dError, kP, kD) + BangBang(error, kL)
-}
+fun SquID(error: Vector, kSq: Double) = error.norm() * sqrt(error.length * kSq)
+fun SquID(error: Double, kSq: Double) = error.sign * sqrt(error.absoluteValue * kSq)
 
-fun SquID(error: Vector, kSq: Double): Vector {
-    return error.norm() * sqrt(error.length * kSq)
-}
-
-fun SquID(error: Double, kSq: Double): Double{
-    return error.sign * sqrt(error.absoluteValue * kSq)
-}
-
-fun BangBang(error: Vector, k: Double): Vector{
-    return error.norm() * k
-}
-
-fun BangBang(error: Double, k: Double): Double{
-    return k * error.sign
-}
+fun BangBang(error: Vector, k: Double) = error.norm() * k
+fun BangBang(error: Double, k: Double) = k * error.sign
